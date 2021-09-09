9b5b7dd8483f17596007fe5745acdfd209e946f1
Crowdbotics <team@crowdbotics.com>
2021-09-09 22:06:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "happygome_30422Identifier",
+  url: "https://happygome_30422Identifier.botics.co",
+  api: "https://happygome_30422Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
